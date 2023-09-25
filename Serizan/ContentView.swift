68c84fa0279eb33acdc02057c5347e9679c5a21e
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-09-25 18:46:17 -0300

    Initial Commit

---
+++ Serizan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Serizan
+//
+//  Created by leonardo Moreira on 25/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Serizan/SerizanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerizanApp.swift
+//  Serizan
+//
+//  Created by leonardo Moreira on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SerizanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
